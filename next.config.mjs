b8b442346e123b54b204485ff8cb9082e2823872
isacostaf <iisabellecostaf@gmail.com>
2025-05-03 17:15:31 -0300

    Corrige configuração do workflow e next.config

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/teste' : '',
+  basePath: '/teste',
+  assetPrefix: '/teste/',
   trailingSlash: true,
 }
 
